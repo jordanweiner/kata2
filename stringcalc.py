e3e658f22b13c0505685530c5a256161a93cd92d
jweiner-balbix <jordan.weiner@balbix.com>
2019-07-09 09:32:35 -0700

    starting over bc I did the first two weeks on accident

--- stringcalc.py
+++ stringcalc.py
@@ -1,12 +1,3 @@
 def add(numbers):
-	sum = 0
-	if (numbers == ""):
-		return sum;
-	else:
-		# go thru chars in string
-		for n in numbers:
-			if ((n == "\n") or (n == ",")):
-				continue
-			else:
-				sum = sum + int(n, 10)
-		return sum
+	
+
