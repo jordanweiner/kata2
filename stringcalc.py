e5b7deb4f2d4d400e7fc74c70c1ad8d2294bbfbe
jweiner-balbix <jordan.weiner@balbix.com>
2019-07-09 09:51:14 -0700

    refactored unknown amount of numbers in string solution

--- stringcalc.py
+++ stringcalc.py
@@ -1,9 +1,6 @@
 def add(numbers):
-	if (numbers == ""):
-		return 0
-	else:
-		sum = 0
-		for num in numbers:
-			sum += int(num)
-		return sum
+	sum = 0
+	for num in numbers:
+		sum += int(num)
+	return sum
 
