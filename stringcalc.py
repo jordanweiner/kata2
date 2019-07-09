bf15e82cedde4bc39fb5dc97b271de4922906f38
jweiner-balbix <jordan.weiner@balbix.com>
2019-07-09 09:35:59 -0700

    test sum returned when zero numbers are in string

--- stringcalc.py
+++ stringcalc.py
@@ -1,3 +1,3 @@
 def add(numbers):
-	
+	return 0
 
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 from stringcalc import add
 
 class TestWeek1(unittest.TestCase):
-	def test_empty_string(self):
+	def test_sum_when_zero_numbers_in_string(self):
 		self.assertEqual(add(""), 0)
 
 if __name__ == '__main__':
