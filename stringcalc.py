21756bb9029bae0ec024fa0e9cf714d20680c3d9
jweiner-balbix <jordan.weiner@balbix.com>
2019-07-25 09:49:23 -0700

    test sum returned when delimeter is specified

--- stringcalc.py
+++ stringcalc.py
@@ -1,7 +1,20 @@
 def add(numbers):
-	sum = 0
-	for num in numbers:
-		if ((num != '\n') and (num != ',')):
-			sum += int(num)
-	return sum
+  if (numbers == ""):
+    return 0
+  elif (len(numbers) < 2):
+    return int(numbers)
+  else:
+    if (numbers[0] == '/' and numbers[1] == '/'):
+      end = numbers.find('\n')
+      delim = numbers[2:end]
+
+      charsList = numbers[(end+1):].split(delim)
+      numList = [int(c) for c in charsList]
+      return sum(numList)
+    else:
+    	total = 0
+    	for num in numbers:
+    		if ((num != '\n') and (num != ',')):
+    			total += int(num)
+    	return total
 
